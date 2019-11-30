58354b12625726c9a164772df2c9f3aed631bdca
imageset11 <17519046+numbersinmyhead@users.noreply.github.com>
2019-11-30 15:21:56 -0700

    What doe cout <<3; do?

---
+++ testprograms/testProg19.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char ** argv) {
+
+    cout << "hello\n";
+    cout << 3; // what will this line do?
+    cout << "hello\n";
+}
+
+
+//the line in question would just print 3 on the screen. That's it.
